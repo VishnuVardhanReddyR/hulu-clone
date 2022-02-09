252374027ad02f0d554124c206194c3d41478d8d
R Vishnu Vardhan Reddy <36665976+VishnuVardhanReddyR@users.noreply.github.com>
2022-02-09 21:54:02 +0530

    ready to deploy

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { 
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon
+} from "@heroicons/react/outline";
+import Image from 'next/image';
+import HeaderItem from './HeaderItem';
 
 function Header() {
+  const headerItems = [
+    { name: "HOME", Icon: HomeIcon },
+    { name: "TRENDING", Icon: LightningBoltIcon },
+    { name: "VERIFIED", Icon: BadgeCheckIcon },
+    { name: "COLLECTIONS", Icon: CollectionIcon },
+    { name: "SEARCH", Icon: SearchIcon },
+    { name: "ACCOUNT", Icon: UserIcon },
+  ]
   return (
-    <header>
-      <h1>Header</h1>
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+      <div className="flex flex-grow justify-evenly max-w-2xl ">
+        {headerItems.map(item => (
+          <HeaderItem key={item.name} title={item.name} Icon={item.Icon} />
+        ))}
+      </div>
+
+      <Image
+        className="object-contain"
+        src="https://links.papareact.com/ua6"
+        width={200}
+        height={100} 
+      />
     </header>
   )
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      "links.papareact.com",
+      "press.hulu.com", 
+      "image.tmdb.org"
+    ]
+  },
 }
